fe3d0d3bad5f7c97c4aa29f1973f8d2719ead704
suba <subalakshmimuthu02@gmail.com>
2023-09-07 14:49:14 +0530

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import EmployeeList from './components/EmployeeList';
+import EmployeeForm from './components/EmployeeForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <div className='container'>
+          <Routes>
+            <Route path='/' element={<EmployeeList />} />
+            <Route path='/create-employee' element={<EmployeeForm />} />
+            <Route path='/edit-employee/:id' element={<EmployeeForm />} />
+          </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
